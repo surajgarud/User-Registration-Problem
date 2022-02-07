c9e2c632c0b446a0c80dc6baee535cc27fa57889
Suraj Garud <surajgarud95@gmail.com>
2022-02-07 15:50:44 +0530

    Enter A Valid First Name

---
+++ UserRegistration/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using UserRegistration;
+
+Console.WriteLine("Hello, World!");
+RegexPracticePro patterns = new RegexPracticePro();
+
+if (patterns.RegexPro("Suraj"))
+{
+    Console.WriteLine("Valid");
+}
+else
+    Console.WriteLine("Invalid");
+Console.ReadKey();
---
+++ UserRegistration/ValidFirstName.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistration
+{
+    class RegexPracticePro
+    {
+        public static string regex = "^[A-Z]{1}[a-z]{2,}$";
+        public bool RegexPro(string sample)
+        {
+            return Regex.IsMatch(sample, regex);
+        }
+    }
+}
